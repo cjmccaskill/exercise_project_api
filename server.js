5c98baf857835ef44c2c625cb699b24747917154
CJ McCaskill <cjmccaskill@gmail.com>
2021-08-17 10:55:52 -0500

    add mongoose connection handler

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
-const { PORT } = process.env || 3000;
+const { PORT, DB_URI } = process.env || 3000;
+
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on("connect", () => console.log(`🤘🏼 Mongo is connected!`));
+db.on("disconnect", () => console.log(`👋🏼 Mongo is disconnected.`));
+db.on("error", (err) => console.log(`❌ Error connecting ${err}`));
 
 app.use(cors());
 app.use(express.json());
